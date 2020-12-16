410507544fc760bdcf5b2aeb381bd102b783bece
fluo10 <fluo10.dev@fireturtle.net>
2020-12-16 05:57:15 +0000

    Comment out duplicated variable

--- bin/test-install.sh
+++ bin/test-install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-function test(){
-	echo "test function"
+function func_b(){
+	echo "func_b@$SCRIPT_DIR"
 }
 
 source $SCRIPT_DIR/../lib/install.sh
+
+func_l
