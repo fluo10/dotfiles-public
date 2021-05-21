7d4693f782baf1b8e5ec38a032925d4faadbff99
fluo10 <fluo10.dev@fireturtle.net>
2021-05-21 07:58:48 +0000

    Split and move dotfiles.sh

--- bin/dotfiles-add-remote.sh
+++ bin/dotfiles-add-remote.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 # セクション一覧の取得
-function load_config() {
-	while read line; do
-		if echo "$line" | grep  remote >/dev/null ; then
-			REMOTE=$line
-		else
-			echo "$REMOTE $line"
-		fi 
-	done < dotfiles.conf
-}
 function reflesh_remote () {
   
   local OPT
@@ -79,4 +70,3 @@ function reflesh_remote () {
     done
   done
 }
-load_config
