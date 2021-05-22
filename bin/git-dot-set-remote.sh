3ed79a27cc821c58be3496cc4cefe040ebc9fe86
fluo10 <fluo10.dev@fireturtle.net>
2021-05-22 14:58:29 +0900

    Add dryrun echo

--- bin/git-dot-set-remote.sh
+++ bin/git-dot-set-remote.sh
@@ -1,71 +1,68 @@
 #!/bin/bash
-function reflesh_remote () {
   
-  local OPT
-  local OPTARG
-  local OPTIND
-  local PREFIX=0 # Set 0 as null
-  local SUBPREFIX=()
-  local IFS=" "
-  local TARGET_BRANCHES=("main" "feature/*")
-  while getopts ps: OPT; do
-      case $OPT in
-  
-          p) # Prefix リモートブランチのプレフィクス
-              PREFIX="$OPTARG"
-              ;;
-  
-          s) # Sub or shered branch
-		SUBPREFIX=("${SUBPREFIX[@]}" "$OPTARG/")
-              ;;
-      esac
-  done
+PREFIX=0 # Set 0 as null
+SUBPREFIX=()
+IFS=" "
+TARGET_BRANCHES=("main" "feature/*")
+while getopts nps: OPT; do
+  case $OPT in
+    n)
+      dryrun_echo="echo "
+      ;;  
+    p) # Prefix リモートブランチのプレフィクス
+      PREFIX="$OPTARG"
+      ;;
 
-  shift `expr $OPTIND - 1`
-    
+    s) # Sub or shered branch
+      SUBPREFIX=("${SUBPREFIX[@]}" "$OPTARG/")
+      ;;
+  esac
+done
 
-  local REMOTE_NAME=$1
-  shift 
-  local REMOTE_URL=$1
+shift `expr $OPTIND - 1`
+  
+
+REMOTE_NAME=$1
+shift 
+REMOTE_URL=$1
+shift
+REMOTE_PUSHURL=()
+while [ "$1" ]; do
+  REMOTE_PUSHURL+=( "$1" )
   shift
-  local REMOTE_PUSHURL=()
-  while [ "$1" ]; do
-    REMOTE_PUSHURL+=( "$1" )
-    shift
-  done
-  if [ $PREFIX = 0 ]; then
-    if [  $REMOTE_NAME = 'origin' ] ; then
-        PREFIX=""
-    else
-        PREFIX="$REMOTE_NAME/"
-    fi
-  fi
-  if [ "`git remote | grep -x  $REMOTE_NAME`" ]; then
-    git remote set-url $REMOTE_NAME "$REMOTE_URL"
-    git config --unset-all remote.$REMOTE_NAME.pushurl
+done
+if [ $PREFIX = 0 ]; then
+  if [  $REMOTE_NAME = 'origin' ] ; then
+      PREFIX=""
   else
-    git remote add $REMOTE_NAME "${REMOTE_URL[0]}"
+      PREFIX="$REMOTE_NAME/"
   fi
-  if [ ${#REMOTE_PUSHURL[@]} -gt 1 ]; then
-    git remote set-url --push $REMOTE_NAME "$REMOTE_URL"
-    for e in ${REMOTE_PUSHURL[@]}; do
-      git remote set-url --add --push $REMOTE_NAME "${e}"
-    done
-  fi
-    
-  
-  git config --unset-all remote.$REMOTE_NAME.fetch
-  git config --unset-all remote.$REMOTE_NAME.push 
-  
-  for e in ${TARGET_BRANCHES[@]}; do
-   git config --add remote.$REMOTE_NAME.fetch "+refs/heads/${e}:refs/remotes/$REMOTE_NAME/$PREFIX${e}"  
-   git config --add remote.$REMOTE_NAME.push "refs/heads/$PREFIX${e}:refs/heads/${e}"
-  done
+fi
+if [ "`git remote | grep -x  $REMOTE_NAME`" ]; then
+  $dryrun_echo git remote set-url $REMOTE_NAME "$REMOTE_URL"
+  $dryrun_echo git config --unset-all remote.$REMOTE_NAME.pushurl
+else
+  $dryrun_echo git remote add $REMOTE_NAME "$REMOTE_URL"
+fi
 
-  for s in ${SUBPREFIX[@]}; do
-    for e in ${TARGET_BRANCHES[@]}; do
-      git config --add remote.$REMOTE_NAME.fetch  "+refs/heads/${s}${e}:refs/remotes/$REMOTE_NAME/${s}${e}"
-      git config --add remote.$REMOTE_NAME.push "refs/heads/${s}${e}" 
-    done
+if [ ${#REMOTE_PUSHURL[@]} -gt 1 ]; then
+  $dryrun_echo git remote set-url --push $REMOTE_NAME "$REMOTE_URL"
+  for e in ${REMOTE_PUSHURL[@]}; do
+    $dryrun_echo git remote set-url --add --push $REMOTE_NAME "${e}"
   done
-}
+fi
+
+$dryrun_echo git config --unset-all remote.$REMOTE_NAME.fetch
+$dryrun_echo git config --unset-all remote.$REMOTE_NAME.push 
+
+for e in ${TARGET_BRANCHES[@]}; do
+  $dryrun_echo git config --add remote.$REMOTE_NAME.fetch "+refs/heads/${e}:refs/remotes/$REMOTE_NAME/$PREFIX${e}"  
+  $dryrun_echo git config --add remote.$REMOTE_NAME.push "refs/heads/$PREFIX${e}:refs/heads/${e}"
+done
+
+for s in ${SUBPREFIX[@]}; do
+  for e in ${TARGET_BRANCHES[@]}; do
+    $dryrun_echo git config --add remote.$REMOTE_NAME.fetch  "+refs/heads/${s}${e}:refs/remotes/$REMOTE_NAME/${s}${e}"
+    $dryrun_echo git config --add remote.$REMOTE_NAME.push "refs/heads/${s}${e}" 
+  done
+done
